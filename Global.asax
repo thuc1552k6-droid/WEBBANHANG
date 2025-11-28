97609628f828678ee89122ec15b152525eca97a6
thuc1552k6-droid <thuc1552k6@gmail.com>
2025-11-28 20:27:32 +0700

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="shopthethao.MvcApplication" Language="C#" %>
---
+++ Global.asax.cs
@@ -0,0 +1,36 @@
+﻿using shopthethao.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace shopthethao
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // QUAN TRỌNG: đảm bảo Razor được bật
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+            // (Không thêm WebFormViewEngine nếu bạn không dùng .aspx/.ascx)
+            // <-- Thêm dòng này để AntiForgery so sánh đúng claim id của Identity
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
+            RoleInitializer.SeedRolesAndAdmin();
+
+        }
+
+    }
+}
